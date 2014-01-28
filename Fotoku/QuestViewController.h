62b4490a06ffdb1990e7190e6b5c4e844b4ec1e7
Olivier van den Biggelaar <ovdbigge@gmail.com>
2014-01-28 23:54:34 +0400

    View Quest Prototype VC

---
+++ Fotoku/QuestViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestViewController.h
+//  Fotoku
+//
+//  Created by Olivier van den Biggelaar on 28/01/14.
+//  Copyright (c) 2014 Olivier Van Den Biggelaar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestViewController : UIViewController
+
+@end
---
+++ Fotoku/QuestViewController.m
@@ -0,0 +1,38 @@
+//
+//  QuestViewController.m
+//  Fotoku
+//
+//  Created by Olivier van den Biggelaar on 28/01/14.
+//  Copyright (c) 2014 Olivier Van Den Biggelaar. All rights reserved.
+//
+
+#import "QuestViewController.h"
+
+@interface QuestViewController ()
+
+@end
+
+@implementation QuestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
